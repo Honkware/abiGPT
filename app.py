a4e8dd72758a520ad9bafe82445a2f44b5bf2731
Honkware <119620994+Honkware@users.noreply.github.com>
2023-03-16 17:47:12 -0500

    Update to GPT-4, improve code formatting and minor enhancements

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, request, jsonify
-from langchain.llms import OpenAI
+from langchain.llms import OpenAIChat
+from langchain.agents import initialize_agent, load_tools
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -9,11 +10,17 @@ load_dotenv()
 app = Flask(__name__)
 
 # Initialize the OpenAI language model
-llm = OpenAI(
-    openai_api_key=os.environ.get('OPENAI_API_KEY'),
-    model_name="gpt-3.5-turbo",
-    temperature=0.1,
+llm = OpenAIChat(
+    openai_api_key=os.environ.get("OPENAI_API_KEY"),
+    model_name="gpt-4",
 )
+
+tools = load_tools(["requests"], llm=llm)
+
+agent = initialize_agent(
+    tools, llm, agent="chat-zero-shot-react-description", verbose=True
+)
+
 
 def convert_abi(abi, abi_type):
     # Template string for the ABI conversion prompt
@@ -30,32 +37,35 @@ def convert_abi(abi, abi_type):
     - Solidity JSON: {{json_example}}
     - Solidity Object: {{obj_example}}
     
-    Output: Include only the converted ABI. Do not add extra descriptors like "Human-Readable:", "Solidity JSON:", "Solidity Object:".
+    Output: Include only the converted raw ABI. Do not add extra descriptors like "Human-Readable:", "Solidity JSON:", "Solidity Object:".
     
     Important: Make sure to output the ABI in the correct {{abi_type}} format. If the requested format is Human-readable, the output should not be in JSON or Object format. Similarly, if the requested format is Solidity JSON or Solidity Object, the output should not be in human-readable format.
     """
-    
+
     # Replace placeholders in the template string with actual values
     abi_detection_conversion_prompt = abi_detection_conversion_template.format(
-        abi=abi, 
-        abi_type=abi_type, 
-        hr_example='function transfer(address to, uint256 amount)', 
-        json_example='{"type": "function", "name": "transfer", "inputs": [{"name": "to", "type": "address"}, {"name": "amount", "type": "uint256"}]}', 
-        obj_example='{type: "function", name: "transfer", inputs: [{name: "to", type: "address"}, {name: "amount", type: "uint256"}]}'
+        abi=abi,
+        abi_type=abi_type,
+        hr_example="function transfer(address to, uint256 amount)",
+        json_example='{"type": "function", "name": "transfer", "inputs": [{"name": "to", "type": "address"}, {"name": "amount", "type": "uint256"}]}',
+        obj_example='{type: "function", name: "transfer", inputs: [{name: "to", type: "address"}, {name: "amount", type: "uint256"}]}',
     )
 
     # Use the OpenAI language model to generate the converted ABI
-    abi_detection_conversion_response = llm(abi_detection_conversion_prompt)
+    abi_detection_conversion_response = agent.run(abi_detection_conversion_prompt)
+
+    # Return the converted ABI
+    return abi_detection_conversion_response
 
     # Remove leading/trailing white space from the response
     return abi_detection_conversion_response.strip()
 
 
-@app.route('/stream_abi', methods=['POST'])
+@app.route("/stream_abi", methods=["POST"])
 def stream_abi():
     # Extract the ABI and ABI type from the POST request
-    abi = request.form['abi']
-    abi_type = request.form['abi_type']
+    abi = request.form["abi"]
+    abi_type = request.form["abi_type"]
 
     # Convert the ABI to the specified type
     converted_abi = convert_abi(abi, abi_type)
@@ -64,10 +74,11 @@ def stream_abi():
     return jsonify({"converted_abi": converted_abi})
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     # Render the index.html template
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
